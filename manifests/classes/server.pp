cd54ff8735a9e3fbd9f13c095cf7480d05e1f661
Tom Barber <tom@analytical-labs.com>
2011-10-13 14:00:56 +0100

    change server

--- manifests/classes/server.pp
+++ manifests/classes/server.pp
@@ -1,4 +1,31 @@
-class apt::server inherits apt {
+class apt::server {
+
+    $root = '/etc/apt'
+    $provider = '/usr/bin/apt-get'
+
+    package { "python-software-properties": }
+
+	file { "sources.list":
+		name => "${root}/sources.list",
+		ensure => present,
+		owner => root,
+		group => root,
+		mode => 644,
+	}
+
+	file { "sources.list.d":
+		name => "${root}/sources.list.d",
+		ensure => directory,
+		owner => root,
+		group => root,
+	}
+	
+	exec { "apt_update":
+		command => "${provider} update",
+		subscribe => [ File["sources.list"], File["sources.list.d"] ],
+		refreshonly => true,
+	}
+	
 	package { 'apache2':
 	    ensure => present,
 	}
