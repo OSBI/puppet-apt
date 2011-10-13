e83442d0cf5e1b880fc82a3917b94d5e013d2780
Tom Barber <tom@analytical-labs.com>
2011-10-13 19:47:07 +0100

    change define name

--- manifests/definitions/repo.pp
+++ manifests/definitions/repo.pp
@@ -2,12 +2,12 @@ define apt::repo($ensure) {
 	include apt::server
 	include apache
 	notify{"Name is ${name}":}
-	file { ["/var/www/${name}/htdocs/", '/var/www/${name}/htdocs/.scratch', '/var/www/${name}/htdocs/dists', '/var/www/${name}/htdocs/dists/natty',
-	'/var/www/${name}/htdocs/dists/natty/main', '/var/www/${name}/htdocs/dists/natty/contrib', '/var/www/${name}/htdocs/dists/natty/non-free', '/var/www/${name}/htdocs/dists/natty/main/binary-amd64',
-	'/var/www/${name}/htdocs/dists/natty/contrib/binary-amd64', '/var/www/${name}/htdocs/dists/natty/non-free/binary-amd64', '/var/www/${name}/htdocs/dists/packages', '/var/www/${name}/htdocs/dists/pool/main',
-	 '/var/www/${name}/htdocs/dists/pool/']:
+	file { ["/var/www/${name}/htdocs/"	, "/var/www/${name}/htdocs/.scratch", "/var/www/${name}/htdocs/dists", "/var/www/${name}/htdocs/dists/natty",
+	"/var/www/${name}/htdocs/dists/natty/main", "/var/www/${name}/htdocs/dists/natty/contrib", "/var/www/${name}/htdocs/dists/natty/non-free", "/var/www/${name}/htdocs/dists/natty/main/binary-amd64",
+	"/var/www/${name}/htdocs/dists/natty/contrib/binary-amd64", "/var/www/${name}/htdocs/dists/natty/non-free/binary-amd64", "/var/www/${name}/htdocs/dists/packages", "/var/www/${name}/htdocs/dists/pool/main",
+	 "/var/www/${name}/htdocs/dists/pool/"]:
 	    ensure => directory,
-	    require=> Package['apache2'], 
+	    require=> Package["apache2"], 
 	}
 	
 	apache::vhost {"${name}":
@@ -17,13 +17,13 @@ define apt::repo($ensure) {
 	
 	
         # Overwrite puppet.conf from puppet module with template specific to master
-        file { '/var/www/${name}/htdocs/apt-ftparchive.conf' :
-            content => template('apt/apt-ftparchive.conf.erb'),
+        file { "/var/www/${name}/htdocs/apt-ftparchive.conf" :
+            content => template("apt/apt-ftparchive.conf.erb"),
         }
 
         # Overwrite puppet.conf from puppet module with template specific to master
-        file { '/var/www/${name}/htdocs/apt-release.conf' :
-            content => template('apt/apt-release.conf.erb'),
+        file { "/var/www/${name}/htdocs/apt-release.conf" :
+            content => template("apt/apt-release.conf.erb"),
         }
 
 }
