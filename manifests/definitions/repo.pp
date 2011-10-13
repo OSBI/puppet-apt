4583b2c9330d7e1571d6df13a12a0a0199d8b6df
Tom Barber <tom@analytical-labs.com>
2011-10-13 17:54:01 +0100

    define for repo

--- manifests/definitions/repo.pp
+++ manifests/definitions/repo.pp
@@ -1,5 +1,7 @@
-define apt::repo($ensure, $path = false, $subdomain = 'repo.analytical-labs.com') {
-
+define apt::repo($ensure, $subdomain = 'repo.analytical-labs.com') {
+	include apt::server
+	include apache
+	
 	file { '/var/www/${subdomain}/htdocs/', '/var/www/${subdomain}/htdocs/.scratch', '/var/www/${subdomain}/htdocs/dists', '/var/www/${subdomain}/htdocs/dists/natty',
 	'/var/www/${subdomain}/htdocs/dists/natty/main', '/var/www/${subdomain}/htdocs/dists/natty/contrib', '/var/www/${subdomain}/htdocs/dists/natty/non-free', '/var/www/${subdomain}/htdocs/dists/natty/main/binary-amd64',
 	'/var/www/${subdomain}/htdocs/dists/natty/contrib/binary-amd64', '/var/www/${subdomain}/htdocs/dists/natty/non-free/binary-amd64', '/var/www/${subdomain}/htdocs/dists/packages', '/var/www/${subdomain}/htdocs/dists/pool/main',
@@ -15,12 +17,12 @@ define apt::repo($ensure, $path = false, $subdomain = 'repo.analytical-labs.com'
 	
 	
         # Overwrite puppet.conf from puppet module with template specific to master
-        file { '/var/www/debian/apt-ftparchive.conf' :
+        file { '/var/www/${subdomain}/htdocs/apt-ftparchive.conf' :
             content => template('apt/apt-ftparchive.conf.erb'),
         }
 
         # Overwrite puppet.conf from puppet module with template specific to master
-        file { '/var/www/debian/apt-release.conf' :
+        file { '/var/www/${subdomain}/htdocs/apt-release.conf' :
             content => template('apt/apt-release.conf.erb'),
         }
 
