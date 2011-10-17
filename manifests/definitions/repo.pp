f2e459c84bf1638b097f162314a625189d9a14d2
Tom Barber <tom@analytical-labs.com>
2011-10-17 17:41:59 +0100

    pool

--- manifests/definitions/repo.pp
+++ manifests/definitions/repo.pp
@@ -4,8 +4,7 @@ define apt::repo($ensure) {
 	notify{"Name is ${name}":}
 	file { ["/var/www/${name}/htdocs/"	, "/var/www/${name}/htdocs/.scratch", "/var/www/${name}/htdocs/dists", "/var/www/${name}/htdocs/dists/natty",
 	"/var/www/${name}/htdocs/dists/natty/main", "/var/www/${name}/htdocs/dists/natty/contrib", "/var/www/${name}/htdocs/dists/natty/non-free", "/var/www/${name}/htdocs/dists/natty/main/binary-amd64",
-	"/var/www/${name}/htdocs/dists/natty/contrib/binary-amd64", "/var/www/${name}/htdocs/dists/natty/non-free/binary-amd64", "/var/www/${name}/htdocs/dists/packages", "/var/www/${name}/htdocs/dists/pool/main",
-	 "/var/www/${name}/htdocs/dists/pool/"]:
+	"/var/www/${name}/htdocs/dists/natty/contrib/binary-amd64", "/var/www/${name}/htdocs/dists/natty/non-free/binary-amd64", "/var/www/${name}/htdocs/dists/packages"]:
 	    ensure => directory,
 	    require=> Package["apache2"], 
 	}
@@ -26,10 +25,10 @@ define apt::repo($ensure) {
             content => template("apt/apt-release.conf.erb"),
         }
 
-	file { ["/srv/repo", "/srv/repo/${name}/pool", "/srv/repo/${name}/pool/s"]:
+	file { ["/srv/repo", "/srv/repo/${name}/pool", "/srv/repo/${name}/pool/main", "/srv/repo/${name}/pool/main/s"]:
 		ensure => directory }
 	
-	file { "/var/www/${name}/htdocs/dists/pool/main":
-    ensure => "/srv/repo/${name}/pool"
+	file { "/var/www/${name}/htdocs/dists/pool/":
+    ensure => "/srv/repo/${name}/pool/"
 	}
 }
