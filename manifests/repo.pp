88d3820b6f8ecc563771a22c7ae412a5efda76f1
Tom Barber <tom@analytical-labs.com>
2012-10-19 14:44:15 +0100

    fix path

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -9,7 +9,6 @@ define apt::repo($ensure) {
   "/var/www/${name}/htdocs/dists/natty/main", "/var/www/${name}/htdocs/dists/natty/contrib", "/var/www/${name}/htdocs/dists/natty/non-free", "/var/www/${name}/htdocs/dists/natty/main/binary-amd64",
   "/var/www/${name}/htdocs/dists/natty/contrib/binary-amd64", "/var/www/${name}/htdocs/dists/natty/non-free/binary-amd64", "/var/www/${name}/htdocs/dists/packages"]:
     ensure => directory,
-    require=> Package["apache2"], 
   }
 
 
